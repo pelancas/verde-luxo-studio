9257656f5434a2a6a7eb07ae087dc314ff0dbb2b
pelancas <isafilara@gmail.com>
2026-05-10 17:10:58 -0300

    Remove subpath base now that custom domain is configured

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/verde-luxo-studio">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projetos" element={<Projects />} />
