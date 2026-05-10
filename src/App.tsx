3db9b81806af7208a6b159f7d6c44514ec11955f
pelancas <isadoraflara@gmail.com>
2026-05-10 16:47:43 -0300

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/verde-luxo-studio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projetos" element={<Projects />} />
